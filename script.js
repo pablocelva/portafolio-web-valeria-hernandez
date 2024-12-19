680349f023b481abf162b889a6a3d0105a45dce1
Pablo Lambert <pablomartin.lambert@gmail.com>
2024-12-18 21:58:18 -0300

    mejoras navbar para diseño mas intuitivo

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 //Menu desplegable
 const menuIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('#nav-menu');
+const navMenuToggle = document.querySelector('#menu-icon-toggle');
+
+function menuToggle() {
+    navMenu.classList.toggle('show');
+}
 
 menuIcon.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
 
 document.addEventListener('click', (e) => {
-    //if (!menuIcon.contains(event.target) && !navMenu.contains(event.target)) {
-    if (!e.target.matches('.menu-icon')) {
+    if (!menuIcon.contains(event.target) && !navMenu.contains(event.target)) {
+    //if (!e.target.matches('.menu-icon')) {
         navMenu.classList.remove('show');
     }
+});
+
+document.addEventListener('scroll', () => {
+    navMenu.classList.remove('show');
 });
 
 //Animacion de texto
@@ -37,3 +46,42 @@ function copiarCorreo() {
         console.error('Error copiando el correo: ', err);
     });
 }
+
+const logo = document.querySelector('.logo');
+//let scrollTimeout;
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        // Si está en la parte superior, muestra el logo
+        logo.classList.remove('hidden');
+    } else {
+        // Si hace scroll, oculta el logo
+        logo.classList.add('hidden');
+    }
+    /*
+    // Oculta el logo al hacer scroll
+    logo.classList.add('hidden');
+
+    // Limpia cualquier temporizador previo
+    clearTimeout(scrollTimeout);
+
+    // Programa la reaparición del logo después de 2 segundos
+    scrollTimeout = setTimeout(() => {
+        logo.classList.remove('hidden');
+    }, 100);*/
+});
+
+let menuIconScrollTimeout;
+
+document.addEventListener('scroll', () => {
+    // Oculta el menú al hacer scroll
+    menuIcon.classList.add('hidden');
+
+    // Limpia cualquier temporizador previo
+    clearTimeout(menuIconScrollTimeout);
+
+    // Programa la reaparición del menú después de 2 segundos
+    menuIconScrollTimeout = setTimeout(() => {
+        menuIcon.classList.remove('hidden');
+    }, 100);
+});
